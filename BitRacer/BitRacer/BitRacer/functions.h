63ca947c06a20fc2ff9d7ca75901b3afc5b5be8b
EEMustafa21 <EEMustafa21@codingburgas.bg>
2023-03-27 02:58:23 +0300

    Add all functions

---
+++ BitRacer/BitRacer/BitRacer/functions.h
@@ -0,0 +1,11 @@
+#pragma once
+using namespace std;
+
+string generateQuestion();
+bool checkAnswer(int a, int b, int op, int answer);
+void car(int sc);
+void greet();
+void CheckeredFlag(int choice);
+void UpperRoad();
+void DownRoad();
+void EndGame(time_t TimeTaken, int score, int choice);
