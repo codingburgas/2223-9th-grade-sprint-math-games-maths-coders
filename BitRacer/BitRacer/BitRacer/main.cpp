4b43f9955408e8a42a0572243f65b0d7c1114023
EEMustafa21 <EEMustafa21@codingburgas.bg>
2023-03-27 03:10:15 +0300

    Get info from the question to put in the checkAnswer function

--- BitRacer/BitRacer/BitRacer/main.cpp
+++ BitRacer/BitRacer/BitRacer/main.cpp
@@ -28,8 +28,6 @@ int main() {
 
 
 
-
-
     for (int i = 0; i < 10; i++) {
         greet();
         CheckeredFlag(choice);
@@ -39,4 +37,45 @@ int main() {
         DownRoad();
         string question = generateQuestion();
         cout << endl << "Question " << (i + 1) << ": " << question << endl;
+
+        int a = 0, b = 0, op = 0;
+        if (question.length() == 10)
+        {
+            if (question.substr(2, 2) == "<<")
+            {
+                op = 3;
+            }
+            else
+            {
+                op = 4;
+            }
+            if (isdigit(question[0])) {
+                a = stoi(question.substr(0, 1));
+            }
+            if (isdigit(question[5])) {
+                b = stoi(question.substr(5, 1));
+            }
+        }
+        else
+        {
+            if (question[2] == '&')
+            {
+                op = 0;
+            }
+            else if (question[2] == '|')
+            {
+                op = 1;
+            }
+            else if (question[2] == '^')
+            {
+                op = 2;
+            }
+
+            if (isdigit(question[0])) {
+                a = stoi(question.substr(0, 1));
+            }
+            if (isdigit(question[4])) {
+                b = stoi(question.substr(4, 1));
+            }
+        }
 }
